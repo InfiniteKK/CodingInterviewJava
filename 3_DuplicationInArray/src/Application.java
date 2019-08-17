dcb7165c95e20b255899aacd3aec57139adce54a
Heqing Kong <kongheqing23@163.com>
2019-08-17 10:19:51 +0800

    add boundary condition determination

--- 3_DuplicationInArray/src/Application.java
+++ 3_DuplicationInArray/src/Application.java
@@ -3,21 +3,28 @@ public class Application {
         if (array.length == 0) {
             System.out.println("Array is empty.");
             return;
-        } else {
-            for (int i = 0; i < array.length; i++) {
-                while (i != array[i]) {
-                    if (array[i] == array[array[i]]) {
-                        System.out.println("One duplication number is " + array[i] + ".");
-                        return;
-                    }
-                    int temp = array[i];
-                    array[i] = array[temp];
-                    array[temp] = temp;
-                }
-                if (i == array.length - 1) {
-                    System.out.println("No duplication number.");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0 || array[i] > array.length - 1){
+                System.out.println("Input error");
+                return;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            while (i != array[i]) {
+                if (array[i] == array[array[i]]) {
+                    System.out.println("One duplication number is " + array[i] + ".");
                     return;
                 }
+                int temp = array[i];
+                array[i] = array[temp];
+                array[temp] = temp;
+            }
+            if (i == array.length - 1) {
+                System.out.println("No duplication number.");
+                return;
             }
         }
 
@@ -26,7 +33,8 @@ public class Application {
     public static void main(String[] args) {
 //        int[] array = {};
 //        int[] array = {2, 3, 1, 0, 4, 5, 6};
-        int[] array = {2, 3, 1, 0, 2, 5, 3};
+//        int[] array = {2, 3, 2, 1, 0, 4, 5, 6};
+        int[] array = {2, 3, 1, 0, 5, 10};
 
 
         duplicationNumber(array);
