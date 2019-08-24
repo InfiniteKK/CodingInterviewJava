88b41d6ff1b4b6536fa8cb1be9eb01ebdfc66e9a
Heqing Kong <kongheqing23@163.com>
2019-08-24 11:08:23 +0800

    commit question 5

--- 5_ReplaceSpaces/src/Application.java
+++ 5_ReplaceSpaces/src/Application.java
@@ -1,41 +1,11 @@
 public class Application {
-    public static void replaceBlank(StringBuilder stringBuilder) {
-        int originalLength = stringBuilder.length();
-        if (originalLength == 0) {
-            System.out.println("Error: input error.");
-            return;
-        }
-        int numOfBlank = 0;
-        for (int i = 0; i < originalLength; i++) {
-            if (stringBuilder.charAt(i) == ' ') {
-                numOfBlank++;
-            }
-        }
-        int newLength = originalLength + 2 * numOfBlank;
-        stringBuilder.setLength(newLength);
-        int originalIndex = originalLength - 1;
-        int newIndex = newLength - 1;
-        while (originalIndex >= 0) {
-            if (stringBuilder.charAt(originalIndex) == ' ') {
-                stringBuilder.setCharAt(newIndex--, '0');
-                stringBuilder.setCharAt(newIndex--, '2');
-                stringBuilder.setCharAt(newIndex--, '%');
-
-            } else {
-                stringBuilder.setCharAt(newIndex--, stringBuilder.charAt(originalIndex));
-            }
-            originalIndex--;
-        }
-        System.out.println(stringBuilder);
-
-    }
-
     public static void main(String[] args) {
 //        String string = "";
 //        String string = " ";
 //        String string = "We  are  happy.";
         String string = "We are happy.";
         StringBuilder stringBuilder = new StringBuilder(string);
-        replaceBlank(stringBuilder);
+        ReplaceBlankDemo demo = new ReplaceBlankDemo();
+        demo.replaceBlank(stringBuilder);
     }
 }
